fe888c8154345530b9a677bf584f694b264d0c88
Albert Perez <albperez@gbox.adnu.edu.ph>
2024-12-31 13:38:54 +0800

    refactor: move values to constant variables

--- ingestion/utils.py
+++ ingestion/utils.py
@@ -11,14 +11,34 @@ from tenacity import (
 
 _logger = logging.getLogger(__name__)
 
+# Retry mechanism
+MIN_WAIT_TIME = 1
+MAX_WAIT_TIME = 5
+MAX_ATTEMPTS = 5
+
 
 @retry(
     before=before_log(_logger, logging.DEBUG),
-    wait=wait_random(1, 5),
-    stop=stop_after_attempt(5),
+    wait=wait_random(MIN_WAIT_TIME, MAX_WAIT_TIME),
+    stop=stop_after_attempt(MAX_ATTEMPTS),
     retry=retry_if_exception_type(requests.RequestException),
 )
 def send_request(session: requests.Session, **kwargs) -> requests.Response:
+    """
+    Sends an HTTP request using the provided session, with automatic retries on failure.
+
+    Arguments:
+        session (requests.Session): The `requests` session object that will be used to send the HTTP request.
+        **kwargs: The parameters that will be passed to the `session.request()` method, such as `url`, `method`, `params`, etc.
+
+    Returns:
+        requests.Response: The HTTP response object returned by the `session.request()` method.
+
+    Raises:
+        requests.RequestException: If the request fails after the maximum number of retry attempts,
+        a `RequestException` will be raised. This includes connection errors, timeout errors,
+        and any other type of error that occurs during the request.
+    """
     response = session.request(**kwargs)
     response.raise_for_status()
     return response
