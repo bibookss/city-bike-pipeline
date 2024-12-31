969e92a4206fcb975f326016b2e07c5682e25bcf
Albert Perez <albperez@gbox.adnu.edu.ph>
2024-12-31 16:35:23 +0800

    refactor: comment out unused models

--- ingestion/models.py
+++ ingestion/models.py
@@ -3,41 +3,51 @@ from typing import List, Optional
 
 from pydantic import BaseModel, Field
 
+class JobParameters(BaseModel):
+    country: Optional[str] = Field(default="all")
+    city: Optional[str]
+    staging_path: str
+    logconf_path: str
 
-class Location(BaseModel):
-    latitude: float
-    longitude: float
-    city: str
-    country: str
-
-
-class Extra(BaseModel):
-    uid: str
-    renting: bool
-    returning: bool
-    last_updated: int
-    address: str
-    post_code: str
-    payment: List[str]
-    payment_terminal: bool = Field(..., alias="payment-terminal")
-    altitude: Optional[float]
-    slots: int
+# class Location(BaseModel):
+#     latitude: float
+#     longitude: float
+#     city: str
+#     country: str
 
 
-class Station(BaseModel):
-    id: str
-    network_id: str
-    name: str
-    latitude: float
-    longitude: float
-    timestamp: datetime
-    free_bikes: int
-    empty_slots: int
-    extra: Extra
+# class Extra(BaseModel):
+#     uid: str
+#     renting: bool
+#     returning: bool
+#     last_updated: int
+#     address: str
+#     post_code: Optional[str]
+#     payment: Optional[List[str]]
+#     payment_terminal: Optional[bool] = Field(..., alias="payment-terminal")
+#     altitude: Optional[float]
+#     slots: Optional[int]
+#     has_ebikes: Optional[bool]
+#     ebikes: Optional[int]
+#     normal_bikes: Optional[int]
 
 
-class Network(BaseModel):
-    id: str
-    name: str
-    location: Location
-    company: List[str]
+# class Station(BaseModel):
+#     id: str
+#     network_id: str
+#     name: str
+#     latitude: float
+#     longitude: float
+#     timestamp: datetime
+#     free_bikes: int
+#     empty_slots: int
+#     extra: Extra
+
+
+# class Network(BaseModel):
+#     id: str
+#     name: str
+#     location: Location
+#     company: List[str]
+
+
